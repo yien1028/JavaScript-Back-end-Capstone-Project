4423bfc5c7d8a16b3832a5422e1592eeafc53e6e
charleneko <130963952+yien1028@users.noreply.github.com>
2024-07-26 16:50:45 +0800

    Add files via upload

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -1,1 +1,23 @@
+// db.js
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
 
+// MongoDB connection URL with authentication options
+let url = `${process.env.MONGO_URL}`;
+
+let dbInstance = null;
+const dbName = "secondChance";
+
+async function connectToDatabase() {
+    if (dbInstance){
+        return dbInstance
+    };
+
+    const client = new MongoClient(url);
+
+    await client.connect();
+    dbInstance = client.db(dbName);
+    return dbInstance;
+}
+
+module.exports = connectToDatabase;
