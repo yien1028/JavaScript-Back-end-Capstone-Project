9e9c90e59273de0a84efebb311c6590432a3fb2c
charleneko <130963952+yien1028@users.noreply.github.com>
2024-07-26 16:59:37 +0800

    Add files via upload

--- sentiment/index.js
+++ sentiment/index.js
@@ -1,1 +1,53 @@
+require('dotenv').config();
+const express = require('express');
+const logger = require('./logger');
+const expressPino = require('express-pino-logger')({ logger });
+const natural = require("natural");
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(expressPino);
+
+// Define the sentiment analysis route
+app.post('/sentiment', async (req, res) => {
+    const { sentence } = req.query;
+
+
+    if (!sentence) {
+        logger.error('No sentence provided');
+        return res.status(400).json({ error: 'No sentence provided' });
+    }
+
+    // Initialize the sentiment analyzer with the Natural's PorterStemmer and "English" language
+    const Analyzer = natural.SentimentAnalyzer;
+    const stemmer = natural.PorterStemmer;
+    const analyzer = new Analyzer("English", stemmer, "afinn");
+
+    // Perform sentiment analysis
+    try {
+        const analysisResult = analyzer.getSentiment(sentence.split(' '));
+
+        let sentiment = "neutral";
+
+        if (analysisResult < 0) {
+            sentiment = "negative";
+        } else if (analysisResult > 0.33) {
+            sentiment = "positive";
+        }
+
+        // Logging the result
+        logger.info(`Sentiment analysis result: ${analysisResult}`);
+        // Responding with the sentiment analysis result
+        res.status(200).json({ sentimentScore: analysisResult, sentiment: sentiment });
+    } catch (error) {
+        logger.error(`Error performing sentiment analysis: ${error}`);
+        res.status(500).json({ message: 'Error performing sentiment analysis' });
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    logger.info(`Server running on port ${port}`);
+});
